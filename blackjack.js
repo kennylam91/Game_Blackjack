9eb8afaac73d6a747f74e43ad464790f926f0692
kennylam91 <phamvanlam1991@gmail.com>
2019-05-22 13:44:37 +0700

    chỉnh sửa lại file js

--- blackjack.js
+++ blackjack.js
@@ -6,12 +6,38 @@ const DEAL_BUTTON = document.getElementById("deal_button");
 const HOLD_BUTTON = document.getElementById("hold_button");
 const PLAYER_POINT = document.getElementById("player_point");
 const HOUSE_POINT = document.getElementById("house_point");
-let player_card_inner_html = "";
-let house_card_inner_html = "";
-let inner_html = "";
-let player_point = 0;
-let house_point = 0;
-let game = {};
+let player;
+let house;
+let dealer;
+START_BUTTON.onclick = startGame;
+DEAL_BUTTON.onclick = function () {
+    dealer.dealCardTo(player);
+    dealer.calculatePointFor(player);
+    PLAYER_CARD.innerHTML = player.cards_html;
+    PLAYER_POINT.innerHTML = player.point;
+};
+HOLD_BUTTON.onclick = function () {
+    console.log("Start");
+    dealer.dealCardTo(house);
+    dealer.dealCardTo(house);
+    dealer.calculatePointFor(house);
+    while (house.point < 17) {
+        dealer.dealCardTo(house);
+        dealer.calculatePointFor(house);
+
+        if (house.point > 21) break;
+    }
+    while (house.point < player.point) {
+        dealer.dealCardTo(house);
+        dealer.calculatePointFor(house);
+        if (house.point > 21) break;
+
+    }
+    HOUSE_CARD.innerHTML = house.cards_html;
+    HOUSE_POINT.innerHTML = house.point;
+
+};
+
 const FULL_CARD_DECK =
     ["2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "Th", "Jh", "Qh", "Kh", "Ah",
         "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "Td", "Jd", "Qd", "Kd", "Ad",
@@ -26,117 +52,70 @@ function Card(value) {
 
 }
 
-function Player() {
+function Player(player) {
+    this.name = player;
     this.cards = [];
     this.point = 0;
+    this.cards_html = "";
     this.receiveCard = function (card) {
         this.cards.push(card.value);
+    };
+    this.setCardHtml = function (html) {
+        this.cards_html += html;
     }
 
-    this.createHtml = function (card) {
-        return card.html;
+
+}
+
+function Dealer() {
+
+    this.card_deck = FULL_CARD_DECK;
+    this.card_number_in_deck = card_number_of_full_deck;
+    this.dealCardTo = function (player) {
+        this.new_card_dealt_index = getRandomNumberFrom0To(this.card_number_in_deck - 1);
+        this.new_card_dealt = this.card_deck[this.new_card_dealt_index];
+        this.card_deck.splice(this.new_card_dealt_index, 1);
+        this.card_number_in_deck = this.card_deck.length;
+        let new_card = new Card(this.new_card_dealt);
+        player.receiveCard(new_card);
+        player.setCardHtml(new_card.html);
+
+
     };
-    this.isThereAceCard = function () {
-        for (i = 0; i < this.cards.length; i++) {
-            if (this.cards[i][0] === "A") return 1;
+    this.calculatePointFor = function (player) {
+        player.point = 0;
+        for (i = 0; i < player.cards.length; i++) {
+            player.point += convertCardValuetoNumber(player.cards[i]);
+        }
+        if (this.isThereAceCard(player)) {
+            if (player.point <= 11) player.point += 10;
+        }
+
+
+    };
+    this.isThereAceCard = function (player) {
+        for (i = 0; i < player.cards.length; i++) {
+            if (player.cards[i][0] === "A") return 1;
         }
         return 0;
     }
-    this.calculatePoint = function () {
-        this.point = 0;
-        for (i = 0; i < this.cards.length; i++) {
-            this.point += convertCardValuetoNumber(this.cards[i]);
-        }
-        if (this.isThereAceCard()) {
-            if (this.point <= 11) this.point += 10;
-        }
-
-
-    };
-
 }
-
-function Game() {
-    this.new_card_dealt;
-    this.card_deck = FULL_CARD_DECK;
-    this.card_dealt_index;
-    this.card_number_in_deck = card_number_of_full_deck;
-    this.dealRandomCard = function () {
-        this.card_dealt_index = getRandomNumberFrom0To(this.card_number_in_deck - 1);
-        this.new_card_dealt = this.card_deck[this.card_dealt_index];
-        this.card_deck.splice(this.card_dealt_index, 1);
-        this.card_number_in_deck = this.card_deck.length;
-
-    };
-}
-
-
-START_BUTTON.onclick = startGame;
-DEAL_BUTTON.onclick = dealCardForPlayer;
-HOLD_BUTTON.onclick = dealCardsForHouse;
 
 
 function startGame() {
     GAME_SCREEN.classList.add("start");
-    game = new Game();
-    player = new Player();
-    house = new Player();
-    game.dealRandomCard();
-    let player_card1 = new Card(game.new_card_dealt);
-    player.receiveCard(player_card1);
-    player.calculatePoint();
-    player_card_inner_html += player.createHtml(player_card1);
-    console.log(player_card_inner_html)
-    game.dealRandomCard();
-    let player_card2 = new Card(game.new_card_dealt);
-    player.receiveCard(player_card2);
-    player.calculatePoint();
-    player_card_inner_html += player.createHtml(player_card2);
-    PLAYER_CARD.innerHTML = player_card_inner_html;
-    PLAYER_POINT.innerHTML = player.point;
-
-
-};
-
-function dealCardForPlayer() {
-    game.dealRandomCard();
-    let player_card = new Card(game.new_card_dealt);
-    player.receiveCard(player_card);
-    player.calculatePoint();
-    player.createHtml(player_card);
-    player_card_inner_html += player.createHtml(player_card);
-    PLAYER_CARD.innerHTML = player_card_inner_html;
+    dealer = new Dealer();
+    player = new Player(player);
+    house = new Player(house);
+    dealer.dealCardTo(player);
+    dealer.dealCardTo(player);
+    dealer.calculatePointFor(player);
+    PLAYER_CARD.innerHTML = player.cards_html;
     PLAYER_POINT.innerHTML = player.point;
 
 
 }
 
-function dealCardsForHouse() {
-    dealSingleCardForHouse();
-    dealSingleCardForHouse();
-    while (house.point < 17) {
-        dealSingleCardForHouse();
-        if (house.point > 21) break;
-
-    }
-    while (house.point < player.point) {
-        dealSingleCardForHouse();
-        if (house.point > 21) break;
-
-    }
-}
-
-function dealSingleCardForHouse() {
-    game.dealRandomCard();
-    let house_card1 = new Card(game.new_card_dealt);
-    house.receiveCard(house_card1);
-    house.calculatePoint(house_card1);
-    house_card_inner_html += house.createHtml(house_card1);
-    HOUSE_CARD.innerHTML = house_card_inner_html;
-    HOUSE_POINT.innerHTML = house.point;
-
-
-}
 
 function getRandomNumberFrom0To(number) {
     return Math.round(Math.random() * number);
