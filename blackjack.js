b037fc853e0adcb2d0621038ddeca4e20a13d45b
kennylam91 <phamvanlam1991@gmail.com>
2019-05-23 16:40:58 +0700

    Thêm các hàm cookie

--- blackjack.js
+++ blackjack.js
@@ -269,5 +269,23 @@ function doAfterThePlayerWin() {
     WINNING_SHOW.classList.remove("house_win");
 
 }
-
+function createCookie(name,value){
+    document.cookie=name+"="+value;
+}
+function readCookie(name){
+    let nameEQ=name+"=";
+    let ca=document.cookie.split(";");
+    for ( let i=0;i<ca.length;i++){
+        let c=ca[i];
+        while(c.charAt(0)===" ") {
+            c=c.substring(1,c.length);
+        }
+        if(c.indexOf(nameEQ)===0)
+            return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
 
